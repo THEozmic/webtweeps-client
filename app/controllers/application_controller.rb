dcc160ce843de9b5173f95da617947ff7765ee19
Michael Ozoemena <michaelozoemena2@gmail.com>
2018-08-13 21:00:15 +0100

    Add crawling and more style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
+require 'net/http'
+
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
+    uri = URI('https://raw.githubusercontent.com/iRaul/web-tweeps/master/README.md')
+    markdown = Net::HTTP.get(uri)
+    @result = markdown.scan(/\| (.*?)\. \|/).map { |x| x.first.split("|") }
+    # binding.pry
     render template: 'index'
   end
+
 end
--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,30 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const tweeps = [
-  {
-    name: "Brendan Eich",
-    twitter: "@BrendanEich",
-    about: "Created JavaScript. Co-founded Mozilla and Firefox."
-  },
-  {
-    name: "Dan Abramov",
-    twitter: "@dan_abramov",
-    about: "Working on React. Co-author of Redux and Create React App."
-  },
-  {
-    name: "Addy Osmani",
-    twitter: "@addyosmani",
-    about: "Working on Google Chrome. Creator of TodoMVC and Yeoman."
-  }
-];
+// const tweeps = [
+//   {
+//     name: "Brendan Eich",
+//     twitter: "@BrendanEich",
+//     about: "Created JavaScript. Co-founded Mozilla and Firefox."
+//   },
+//   {
+//     name: "Dan Abramov",
+//     twitter: "@dan_abramov",
+//     about: "Working on React. Co-author of Redux and Create React App."
+//   },
+//   {
+//     name: "Addy Osmani",
+//     twitter: "@addyosmani",
+//     about: "Working on Google Chrome. Creator of TodoMVC and Yeoman."
+//   }
+// ];
 
 const Person = ({ person }) => {
-  const { name, twitter, about } = person;
+  const name = person[0];
+  const twitter = person[1].match(/\[(.*?)\]/)[1];
+  const about = person[2];
   return (
     <div className="person">
       <span className="person__name">{name}</span>
-      <a className="person__handle" href={`https://twitter.com/${twitter}`}>
+      <a
+        className="person__handle"
+        target="_blank"
+        href={`https://twitter.com/${twitter}`}
+      >
         {twitter}
       </a>
       <p className="person__about">{about}</p>
@@ -34,11 +40,16 @@ const Person = ({ person }) => {
 
 class Home extends React.Component {
   render() {
+    console.log(this.props.result);
+    const tweeps = this.props.result;
+    window.tweeps = tweeps;
     return (
       <React.Fragment>
         <div className="persons">
           {tweeps.map(person => {
-            return <Person key={person.twitter} person={person} />;
+            return (
+              <Person key={person[1].match(/\[(.*?)\]/)[1]} person={person} />
+            );
           })}
         </div>
       </React.Fragment>
